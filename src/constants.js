adc177fa55c049633921644d713ff627e9b82fbb
Patrik Toma <patotoma@gmail.com>
2017-10-05 21:09:27 +0200

    add constants.js

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const nodeEnv = process.env.NODE_ENV;
+export const storageKeyPrefix = process.env.REACT_APP_STORAGE_KEY_PREFIX;
+export const apiBase = process.env.REACT_APP_API_BASE;
+export const screenBreakpoints = {
+  lg: 1170,
+  md: 992,
+  sm: 768,
+  xs: 376,
+};
