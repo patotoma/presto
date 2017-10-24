bbd9c815bd3ec929ff5da79f1deadf3bfd4e6c49
Patrik Toma <patotoma@gmail.com>
2017-10-24 23:39:35 +0200

    remove unused tests

--- src/components/ErrorBoundary/ErrorBoundary._test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { ErrorBoundary } from './ErrorBoundary.js';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ErrorBoundary />, div);
-});
